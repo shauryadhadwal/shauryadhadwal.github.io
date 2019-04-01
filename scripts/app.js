6ecb9da470a3405b207a4ff5505ab70f30645e4e
Shaurya Dhadwal <shauryadhadwal@gmail.com>
2019-04-02 00:39:48 +0530

    Parallax Events

--- scripts/app.js
+++ scripts/app.js
@@ -1,34 +1,40 @@
-var stage;
-var squareSize = 60;
+$(window).scroll(function () {
+    var scrollVal = $(this).scrollTop();
+    //Move left on scroll down
+    $("#intro-name").css("transform", "translate(-" + (scrollVal / 10) + "%, -50%)");
 
-function init() {
-    //resize canvas to full width and height
-    var canvas = document.getElementsByTagName('canvas')[0];
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-    stage = canvas.getContext('2d');
+    //Move up on scroll down
+    // $("#intro-programmer").css("transform", "translateY(-" + (50 + scrollVal / 10) + "%)");
+    $("#intro-programmer").css("transform", "translate(" + (scrollVal / 50) + "%,-50%)");
 
-    var rows = Math.floor(canvas.height / squareSize);
-    var cols = Math.floor(canvas.width / squareSize);
-    console.log(rows, cols);
+    //Move up on scroll down
+    $("#intro-summary").css("transform", "translate(0,-" + (scrollVal / 2) + "%)");
 
-    var squares = new Array(rows);
+    //Move left on scroll down
+    $("#intro .intro-bg-split--left").css("transform", "translateX(-" + (scrollVal / 10) + "%)");
 
-    //Loop to create squares
-    for (i = 0; i <= cols; i++) {
-        squares[i] = new Array(cols);
-        for (j = 0; j <= rows; j++) {
-            stage.strokeStyle = "rgb(195, 206, 224)";
-            squares[i][j] = stage.strokeRect(squareSize * i, squareSize * j, squareSize, squareSize);
-        }
+    //Move left on scroll down
+    $("#intro .intro-bg-split--right").css("transform", "translateX(" + (scrollVal / 10) + "%)");
+
+    var aboutTop = $("#about").offset().top;
+    if (aboutTop - scrollVal < 0) {
+        //Move left on scroll down
+        $("#about .img-container").css("transform", "translate(" + ((aboutTop - scrollVal)/40) + "%, -50%)");
+        // console.log('[About]', "translateX(-" + ((aboutTop - scrollVal)/50) + "%)") ;
+    }
+    var technologyTop = $("#technologies").offset().top;
+    if (technologyTop - scrollVal < 0) {
+        //Move left on scroll down
+        $("#technologies .img-container").css("transform", "translate(" + ((technologyTop - scrollVal)/40) + "%, -50%)");
+        // console.log('[About]', "translateX(-" + ((aboutTop - scrollVal)/50) + "%)") ;
     }
 
-    squares[5][5] = null;
-}
 
-function randomColor() {
-    var x = Math.floor(Math.random() * 256);
-    var y = Math.floor(Math.random() * 256);
-    var z = Math.floor(Math.random() * 256);
-    return "rgb(" + x + "," + y + "," + z + ")";
-}
+});
+
+$("#about").mousemove(function (e) {
+    // var valueX = (e.pageX * -1 / 15);
+    // var valueY = (e.pageY * -1 / 15);
+
+    // $("#about .mouse-object").css("transform", "translate3d(" + valueX + "px," + valueY + "px,0)");
+})
